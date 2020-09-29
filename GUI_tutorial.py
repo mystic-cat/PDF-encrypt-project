625908f2e0c36c36fdf47d2dff061cbea215cf6f
mystic-cat <68018136+mystic-cat@users.noreply.github.com>
2020-09-29 11:54:14 -0700

    init

---
+++ GUI_tutorial.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+
+window = tk.Tk()
+greeting = tk.Label(text = "Hello World") #Will create a text label
+greeting.pack() #Shrinks the window as small as it can 
+window.mainloop() # need this at the end of the python script to create the GUI
