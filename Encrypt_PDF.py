e39fbf2cbac06763cbe6af9201fd3bd290fd3207
mystic-cat <68018136+mystic-cat@users.noreply.github.com>
2020-10-19 09:33:19 -0700

    Update Encrypt_PDF.py

--- Encrypt_PDF.py
+++ Encrypt_PDF.py
@@ -6,7 +6,7 @@ from tkinter import ttk
 # filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
 win = tk.Tk()
 win.title("PDF Encryptor")
-win.resizable(True, True)
+win.resizable(False, False)
 
 master = ttk.Labelframe(win, text = "1 -> 4")   # Master Frame
 master.grid(column = 0, row = 0, padx = 8, pady = 4)
@@ -17,23 +17,23 @@ btn_choose = ttk.Button(frm_choose, text = "\n Choose PDF file to encrypt \n")
 btn_choose.pack(padx = 10, pady = 10)
 
 frm_pass = ttk.Labelframe(master, text = "2")   # Frame 2
-frm_pass.grid(column = 1, row = 0, padx = 10, pady = 2, ipadx = 2, ipady = 10)
+frm_pass.grid(column = 1, row = 0, padx = 10, pady = 2, ipadx = 2, ipady = 8)
 lbl_pass = ttk.Label(frm_pass, text = "Set Password:")
 lbl_pass.grid(column = 0, row = 0, sticky = "w")
 entr_pass = ttk.Entry(frm_pass)
 entr_pass.grid(column = 0, row = 1, padx = 8, pady = 10, sticky = "w")
 
 frm_output = ttk.Labelframe(master, text = "3") # Frame 3
-frm_output.grid(column = 2, row = 0, padx = 8, pady = 10)
+frm_output.grid(column = 2, row = 0, padx = 8, pady = 10, ipadx = 2, ipady = 13)
 rad_output_default = ttk.Radiobutton(frm_output, text = "Output to default folder")
-rad_output_default.grid(column = 0, row = 0, sticky = "w")
+rad_output_default.grid(column = 0, row = 0, sticky = "sw", pady = 6)
 rad_output_choose = ttk.Radiobutton(frm_output, text = "Output to a different folder")
-rad_output_choose.grid(column = 0, row = 1, sticky = "w")
+rad_output_choose.grid(column = 0, row = 1, sticky = "sw")
 
 frm_encrypt = ttk.LabelFrame(master, text = "4")    #Frame 4
 frm_encrypt.grid(column = 3, row = 0, padx = 8, pady = 10)
-btn_encrypt = ttk.Button(frm_encrypt, text = "Encrypt")
-btn_encrypt.pack(padx = 10, pady = 10)
+btn_encrypt = ttk.Button(frm_encrypt, text = "\nEncrypt\n")
+btn_encrypt.pack(padx = 10, pady = 12)
 
 
 
