351627153046d32a9c7d0a5e1762bcf7f4f3ff8a
mystic-cat <68018136+mystic-cat@users.noreply.github.com>
2020-10-19 17:31:23 -0700

    Tool tips

--- Encrypt_PDF.py
+++ Encrypt_PDF.py
@@ -19,7 +19,45 @@ def _about_msg_box():
 
 
 """ 
-***************************GUI*********************
+***************************GUI Begins*********************
+"""
+"""
+***************************************Tool Tips Begins***********************************
+"""
+
+class ToolTip(object):
+    def __init__(self, widget, tip_text=None):
+        self.widget = widget
+        self.tip_text = tip_text
+        widget.bind('<Enter>', self.mouse_enter)           # bind mouse events
+        widget.bind('<Leave>', self.mouse_leave)
+
+    def mouse_enter(self, _event):                       
+        self.show_tooltip()
+        
+    def mouse_leave(self, _event):
+        self.hide_tooltip()
+
+    def show_tooltip(self):
+        if self.tip_text:
+            x_left = self.widget.winfo_rootx()                          # get widget top-left coordinates
+            y_top = self.widget.winfo_rooty() - 18                      # place tooltip above widget or it flickers
+            
+            self.tip_window = tk.Toplevel(self.widget)                  # create Toplevel window; parent=widget
+            self.tip_window.overrideredirect(True)                      # remove surrounding toolbar window
+            self.tip_window.geometry("+%d+%d" % (x_left, y_top))        # position tooltip 
+    
+            label = tk.Label(self.tip_window, text=self.tip_text, justify=tk.LEFT,
+                          background="#ffffe0", relief=tk.SOLID, borderwidth=1,
+                          font=("tahoma", "8", "normal"))
+            label.pack(ipadx=1)
+
+    def hide_tooltip(self):
+        if self.tip_window:
+            self.tip_window.destroy()       
+
+"""
+***************************************Tool Tips Ends***********************************
 """
 win = tk.Tk()
 win.title("PDF Encryptor")
@@ -43,6 +81,7 @@ frm_choose = ttk.Labelframe(master, text = "1") # Frame 1
 frm_choose.grid(column = 0, row = 0, padx = 10, pady = 20)
 btn_choose = ttk.Button(frm_choose, text = "\n Choose PDF file to encrypt \n")
 btn_choose.pack(padx = 10, pady = 10)
+ToolTip(btn_choose, "Select the PDF file that you would like to encrypt")  # Tool tip
 
 frm_pass = ttk.Labelframe(master, text = "2")   # Frame 2
 frm_pass.grid(column = 1, row = 0, padx = 10, pady = 2, ipadx = 2, ipady = 8)
@@ -51,21 +90,23 @@ lbl_pass.grid(column = 0, row = 0, sticky = "w")
 entr_pass = ttk.Entry(frm_pass)
 entr_pass.grid(column = 0, row = 1, padx = 8, pady = 10, sticky = "w")
 entr_pass.focus()   #Place cursor into password entry line when the app starts
+ToolTip(entr_pass, "Please enter the password that you \nwould like to have on the encrypted PDF")  # Tool tip
 
 frm_output = ttk.Labelframe(master, text = "3") # Frame 3
 frm_output.grid(column = 2, row = 0, padx = 8, pady = 10, ipadx = 2, ipady = 13)
-rad_output_default = ttk.Radiobutton(frm_output, text = "Output to default folder")
+rad_output_default = ttk.Radiobutton(frm_output, text = "Save copy to default folder")
 rad_output_default.grid(column = 0, row = 0, sticky = "sw", pady = 6)
-rad_output_choose = ttk.Radiobutton(frm_output, text = "Output to a different folder")
+rad_output_choose = ttk.Radiobutton(frm_output, text = "Save copy to a different folder")
 rad_output_choose.grid(column = 0, row = 1, sticky = "sw")
+ToolTip(frm_output, "Please indicate whether you would like the option to choose where the copied encrypted file is saved") # Tool tip
 
 frm_encrypt = ttk.LabelFrame(master, text = "4")    #Frame 4
 frm_encrypt.grid(column = 3, row = 0, padx = 8, pady = 10)
 btn_encrypt = ttk.Button(frm_encrypt, text = "\nEncrypt\n")
 btn_encrypt.pack(padx = 10, pady = 12)
-
+ToolTip(btn_encrypt, "Press this to begin encryption")  # Tool tip
 
 win.mainloop()
 """
-***************************GUI*********************
+***************************GUI Ends*********************
 """
